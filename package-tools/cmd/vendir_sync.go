e87dc8135cb5a63b3f161f6a6f0a8ac27f877931
Nicholas Seemiller <nseemiller@vmware.com>
2022-08-23 14:31:11 -0600

    update module

--- package-tools/cmd/vendir_sync.go
+++ package-tools/cmd/vendir_sync.go
@@ -13,8 +13,8 @@ import (
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
 
-	"github.com/vmware-tanzu/build-tooling-for-integrations/package-tools/constants"
-	"github.com/vmware-tanzu/build-tooling-for-integrations/package-tools/utils"
+	"github.com/seemiller/release/package-tools/constants"
+	"github.com/seemiller/release/package-tools/utils"
 )
 
 // packageVendirSyncCmd is for sync the package
--- package-tools/main.go
+++ package-tools/main.go
@@ -3,7 +3,7 @@
 
 package main
 
-import "github.com/vmware-tanzu/build-tooling-for-integrations/package-tools/cmd"
+import "github.com/seemiller/release/package-tools/cmd"
 
 func main() {
 	cmd.Execute()
